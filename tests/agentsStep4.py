9bd58f92f1ad82b69f5f673dacd9bedc2a03a444
Sathvik Nair <sathviknair@berkeley.edu>
2017-09-24 16:42:02 -0700

    setup step in tests

--- tests/agentsStep4.py
+++ tests/agentsStep4.py
@@ -6,35 +6,39 @@ test = {
       'cases': [
         {
           'code': r"""
-                                        	        >>> agents.history = History.History(agents.rock_strategy, agents.reflexive_strategy)
-                                                    >>> assert round(Simulator.simulator(agents.rock_strategy, agents.reflexive_strategy, history_storage = agents.history, simulation_count = 1000, silent = True)[1]) == 1
-                                                    >>> agents.history = History.History(agents.scissors_strategy, agents.reflexive_strategy)
-                                                    >>> assert round(Simulator.simulator(agents.scissors_strategy, agents.reflexive_strategy, history_storage = agents.history, simulation_count = 1000, silent = True)[1]) == 1
+            >>> agents.history = History.History(agents.rock_strategy, agents.reflexive_strategy)
+            >>> assert round(Simulator.simulator(agents.rock_strategy, agents.reflexive_strategy, history_storage = agents.history, simulation_count = 1000, silent = True)[1]) == 1
+            >>> agents.history = History.History(agents.scissors_strategy, agents.reflexive_strategy)
+            >>> assert round(Simulator.simulator(agents.scissors_strategy, agents.reflexive_strategy, history_storage = agents.history, simulation_count = 1000, silent = True)[1]) == 1
           """,
           'hidden': False,
           'locked': False
         },
         {
           'code': r"""
-                                        			>>> for _ in range(trials):
-                                                    ... rates = np.random.random(3)
-                                                    ... rates /= rates.sum()
-                                                    ... strategy1 = agents.triple_biased_strategy(rates[0], rates[1], rates[2])
-                                                    ... strategy2 = agents.reflexive_strategy
-                                                    ... agents.history = History.History(strategy1, strategy2)
-                                                    ... results = Simulator.simulator(strategy1, strategy2, history_storage = agents.history, simulation_count = 1000, silent = True)
-                                                    ... if results[1] > results[0]:
-                                                    ...... wins += 1
-                                                    ... if results[0] - results[1] > 0:
-                                                    ......  assert results[0] - results[1] < 0.1
-                                                    >>> assert wins/trials > 0.9, "Your strategy does not win often enough"
+            >>> for _ in range(trials):
+            ...    rates = np.random.random(3)
+            ...    rates /= rates.sum()
+            ...    strategy1 = agents.triple_biased_strategy(rates[0], rates[1], rates[2])
+            ...    strategy2 = agents.reflexive_strategy
+            ...    agents.history = History.History(strategy1, strategy2)
+            ...    results = Simulator.simulator(strategy1, strategy2, history_storage = agents.history, simulation_count = 1000, silent = True)
+            ...    if results[1] > results[0]:
+            ...        wins += 1
+            ...    if results[0] - results[1] > 0:
+            ...        assert results[0] - results[1] < 0.1
+            >>> assert wins/trials > 0.9, "Your strategy does not win often enough"
           """,
           'hidden': False,
           'locked': False
         }
       ],
       'scored': True,
-      'setup': '',
+      'setup': r"""
+        >>> import agents as agents
+        >>> import History as History
+        >>> import Simulator as Simulator
+      """,
       'teardown': '',
       'type': 'doctest'
     }
